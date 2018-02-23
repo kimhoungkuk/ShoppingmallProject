df83e6cb27bfa902b3beddf2a8d2d1df92e4fe35
doproperly <themostfamousthing@gmail.com>
2018-02-23 18:24:29 +0900

    first commit

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
    - JDK : 1.8
    - TOMCAT : 8.0
    - ORACLE : 11g
+   
+ * LOG 설정
+ 
+  - SLF4J , Logback 사용
+  - Logback를 사용해야 하는 이유 : goo.gl/p9M8C 주소 참조
+ 
+ 
--- src/main/java/com/project/shop/backoffice/controller/ProductController.java
+++ src/main/java/com/project/shop/backoffice/controller/ProductController.java
@@ -1,26 +1,25 @@
 package com.project.shop.backoffice.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.project.shop.service.ProductService;
 import com.project.shop.config.Config;
 import com.project.shop.model.Product;
+import com.project.shop.service.ProductService;
 
-import java.sql.SQLException;
-import java.util.List;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-@Controller 
+@Controller
 public class ProductController {
 
-    private Log log = LogFactory.getLog(this.getClass());
-    
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+	
     @Autowired
     private ProductService productService;
 
@@ -30,9 +29,11 @@ public class ProductController {
     @RequestMapping(value = "/admin/product")
     public ModelAndView productList(ModelMap modelMap) throws SQLException{
     	
+    	LOGGER.info("hello SLF4J");
+    	
     	List<Product>  product = productService.getProductList();
 
-    	log.debug(product);
+    	Product product2 = product.get(0);
     	
     	System.out.println(Config.getString("hello"));
     	
