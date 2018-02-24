66799cc6fa3ee4e5cd1622dbb92e80e82aa0fb2e
doproperly <themostfamousthing@gmail.com>
2018-02-24 13:23:54 +0900

    - test

--- src/main/java/com/project/shop/backoffice/controller/ProductController.java
+++ src/main/java/com/project/shop/backoffice/controller/ProductController.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-package com.project.shop.backoffice.controller;
-
-import java.sql.SQLException;
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.project.shop.config.Config;
-import com.project.shop.model.Product;
-import com.project.shop.service.ProductService;
-
-@Controller
-public class ProductController {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
-	
-    @Autowired
-    private ProductService productService;
-
-    /**
-     * 상품 리스트.
-     */
-    @RequestMapping(value = "/admin/product")
-    public ModelAndView productList(ModelMap modelMap) throws SQLException{
-    	
-    	LOGGER.info("hello SLF4J");
-    	
-    	List<Product>  product = productService.getProductList();
-
-    	Product product2 = product.get(0);
-    	
-    	System.out.println(Config.getString("hello"));
-    	
-    	return new ModelAndView("home");
-    }
-
-}
-=======
 package com.project.shop.backoffice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -87,4 +42,3 @@ public class ProductController {
     }
 
 }
->>>>>>> branch 'master' of https://github.com/kimhoungkuk/ShoppingmallProject
